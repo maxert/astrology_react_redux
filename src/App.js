ea1cc26e226c1d7b7871357674b475905585e5c3
maxert <max97-12@ukr.net>
2020-02-17 10:53:02 +0200

    first

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './styles.scss';
+
+function Home() {
+  return (
+    <div className="container_home">
+      
+    </div>
+    
+  );
+}
+
+export default Home;
