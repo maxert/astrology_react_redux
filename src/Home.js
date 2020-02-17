dfd61b66568be5d716f57432af6cd195c5a8f02f
maxert <max97-12@ukr.net>
2020-02-17 21:41:51 +0200

    update

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,13 @@
 import React from "react";
 import "./styles.scss";
-import { BrowserRouter as Router, Switch, Route, NavLink  } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  NavLink
+} from "react-router-dom";
+import { SvgLoader, SvgProxy } from "react-svgmt";
+import Search from "./addElement/search";
 function About() {
   return <h2>Home</h2>;
 }
@@ -12,26 +19,39 @@ function Home() {
     <div className="container_home">
       <Router>
         <div className="container_center">
-          <div>
+          <div className="container_left">
             <div className="menu_head">
               <div className="elipse">A</div>
               <div className="text_name">АстроТаро</div>
             </div>
             <ul className="menu_list">
               <li>
-                <NavLink  to="/" exact={true} activeClassName='active'>
+                <NavLink to="/" exact={true} activeClassName="active">
+                  <SvgLoader path="./img/Union.svg">
+                    <SvgProxy selector="#co" />
+                  </SvgLoader>
                   <div className="img_list">
-                    <img src="./img/Home.svg" alt="Картинка"/>
+                    <SvgLoader path="./img/Home.svg">
+                      <SvgProxy selector="#co" />
+                    </SvgLoader>
                   </div>
                   <div className="text_list">Главная</div>
-                </NavLink >
+                </NavLink>
               </li>
               <li>
-                <NavLink  to="/element">1</NavLink >
+                <NavLink to="/element">
+                  <SvgLoader path="./img/Union.svg">
+                    <SvgProxy selector="#co" />
+                  </SvgLoader>
+                  1
+                </NavLink>
               </li>
             </ul>
           </div>
-          <div className="container_left">
+          <div className="container_right">
+            <div className="search_container">
+              <Search></Search>
+            </div>
             <Switch>
               <Route exact path="/">
                 <About>1</About>
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import Home from "./Home";
 import * as serviceWorker from "./serviceWorker";
+import 'semantic-ui-css/semantic.min.css';
+import "./styles.scss";
 
 ReactDOM.render(<Home />, document.getElementById("root"));
 
